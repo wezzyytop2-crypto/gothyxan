db9e4681dcd96d00c9980404032529d73aa0dc52
mellory <wezzyytop2@icloud.com>
2025-12-11 22:32:34 +0200

    значиетнльыные изменения сайта

---
+++ js/firebase-config.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB7uFK25renleU9ZkTpDbY4zQo7hrCh-B0",
+    authDomain: "gothyxan.firebaseapp.com",
+    projectId: "gothyxan",
+    storageBucket: "gothyxan.firebasestorage.app",
+    messagingSenderId: "329511907152",
+    appId: "1:329511907152:web:2afaf437e6637581a30655",
+    measurementId: "G-FEH7NJX4MX"
+};
+// »нициализаци€ Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Ёкспорт необходимых сервисов
+const db = firebase.firestore();
